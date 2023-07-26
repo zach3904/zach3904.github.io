daa0f83ba60e6a5ae284d5a19c47de03e3a74d90
Zach Richards <zach3904@gmail.com>
2023-07-25 18:23:46 -0700

    Add cx search description

--- js/content.js
+++ js/content.js
@@ -2,10 +2,10 @@ var content = {
 	"webProjects": [
 		{
 			"title": "Octo CX Search",
-			"url": false,
+			"url": "https://www.octo.us/products/octocx/cxsearch/",
 			"imgName": "cx-search",
 			"imgCount": 12,
-			"description": "",
+			"description": "CX Search is a tool for searching and discovering data. This was my initial and primary project while working at Octo. I built most of the UI layout and components. Also maintained a simple component library for the other devs to use. JSS was used for styling and typescript to keep the JS clean.",
 			"skills": [
 				"React",
         "Typescript",
@@ -21,14 +21,14 @@ var content = {
 			"imgCount": 7,
 			"description": "UI Library was a pattern library I created while at Porch to act as a source of truth for commonly used components. On the design side, I used Lingo to house an organized collection of Sketch symbols utilizing overrides to define their uses. And on the engineering side, developers had access to a repository of identically styled React components that could be viewed in Storybook.",
 			"skills": [
-                "HTML",
-                "LESS",
+        "HTML",
+        "LESS",
 				"Javascript ES6",
 				"React",
 				"Sketch",
 				"Lingo",
 				"Storybook"
-            ]
+      ]
 		},
 		{
 			"title": "Porch Pro",
@@ -37,14 +37,14 @@ var content = {
 			"imgCount": 5,
 			"description": "Overview of various projects and applications I worked on while on the \"Pro\" team at Porch. The majority of my contributions were collecting data from API endpoints and building out designs.",
 			"skills": [
-                "HTML",
-                "LESS",
+        "HTML",
+        "LESS",
 				"Javascript ES6",
 				"React",
 				"Responsive Design",
 				"Bootstrap",
 				"API"
-            ]
+      ]
 		},
 		{
 			"title": "ActivProgress",
@@ -54,13 +54,13 @@ var content = {
 			"description": "ActivProgress is a data heavy student assessment and reporting web application my team was tasked with revitalizing. We updated the front end with a fresher design and rebuilt most of the functionality using AngularJS. This included re-tying up the front end to the existing data retreival API.",
 			"skills": [
 				"Design",
-                "HTML5",
-                "CSS3",
-                "Javascript",
-                "AngularJS",
-                "Media Queries",
-                "Dynamic Content"
-            ]
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "AngularJS",
+        "Media Queries",
+        "Dynamic Content"
+      ]
 		},
 		{
 			"title": "ClassFlow",
@@ -69,13 +69,13 @@ var content = {
 			"imgCount": 3,
 			"description": "ClassFlow is a classroom tool for building and presenting lessons. I helped out in building the UI and functionality for the lesson presenter; an open area where \"cards\" could be placed, rotated, scaled, and even have their content changed as well as be shown to the classroom. The front end was structured around KendoUI.",
 			"skills": [
-                "HTML5",
-                "CSS3",
-                "Javascript",
-                "KendoUI",
-                "Interactivity",
-                "Dynamic Content"
-            ]
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "KendoUI",
+        "Interactivity",
+        "Dynamic Content"
+      ]
 		},
 		{
 			"title": "Pagliacci Pizza - Mobile Site",
@@ -85,13 +85,13 @@ var content = {
 			"description": "Getting to work for one of Seattle's favorite pizza places was quite an honor! I was given a simple requirement list and told to have fun with it.  It was my first start-to-finish project using jQuery Mobile. It all came together smoothly and the client was very happy with the result! Winning.Getting to work for one of Seattle's favorite pizza places was quite an honor! I was given a simple requirement list and told to have fun with it.  It was my first start-to-finish project using jQuery Mobile. It all came together smoothly and the client was very happy with the result! Winning.Getting to work for one of Seattle's favorite pizza places was quite an honor! I was given a simple requirement list and told to have fun with it.  It was my first start-to-finish project using jQuery Mobile. It all came together smoothly and the client was very happy with the result! Winning.",
 			"skills": [
 				"Design",
-                "jQuery Mobile",
-                "HTML5",
-                "CSS3",
-                "jQuery",
-                "Media Queries",
-                "AJAX"
-            ]
+        "jQuery Mobile",
+        "HTML5",
+        "CSS3",
+        "jQuery",
+        "Media Queries",
+        "AJAX"
+      ]
 		},
 		{
 			"title": "Sizzling Platter",
@@ -101,13 +101,13 @@ var content = {
 			"description": "Sizzling Platter was the largest project I worked on at Clocktower Media. It consisted of a main corporate site, 6 individual brand sites, and a customer reward tracking/management site. The one you see here is the main corporate site. I did the designs for all of them and built the front end, including all the HTML &amp; CSS with a good portion of the Javascript.",
 			"skills": [
 				"Full Site Design",
-                "HTML5",
-                "CSS3",
-                "jQuery",
-                "Media Queries",
-                "Bootstrap",
-                "AJAX"
-            ]
+        "HTML5",
+        "CSS3",
+        "jQuery",
+        "Media Queries",
+        "Bootstrap",
+        "AJAX"
+      ]
 		},
 		{
 			"title": "Value Passport",
@@ -117,12 +117,12 @@ var content = {
 			"description": "Value Passport is a customer rewards tracking site built for Sizzling Platters. While much of the site's functionality was already built when I was brought, I did perform lots of work on it including a full site design, HTML/CSS, and major tweaking/additions to the Javascript. This site is very data driven with a focus on function over form. Heavy AJAX and data handling via Javascript was used making working on this one of my greater learning experiences.",
 			"skills": [
 				"Full Site Design",
-                "HTML5",
-                "CSS3",
-                "jQuery",
-                "AJAX",
-                "Bootstrap"
-            ]
+        "HTML5",
+        "CSS3",
+        "jQuery",
+        "AJAX",
+        "Bootstrap"
+      ]
 		},
 		{
 			"title": "Sizzling Platter Corporate Brands",
@@ -132,12 +132,12 @@ var content = {
 			"description": "These were the final parts in the Sizzling Platter project. The sites needed to have a very obvious connection while matching their brands' flair and be noticably unique to each other. Each site uses the same base CSS and is overridden with their own styles. This really forced me to think hard about each change I made to the CSS as it would affect all sites and had to be easily overridden. Also, each site is responsive to help scale the big image feel to smaller screens.",
 			"skills": [
 				"Full Site Design",
-                "HTML5",
-                "CSS3",
-                "jQuery",
-                "Media Queries",
-                "Bootstrap"
-            ]
+        "HTML5",
+        "CSS3",
+        "jQuery",
+        "Media Queries",
+        "Bootstrap"
+      ]
 		},
 		{
 			"title": "Exvana",
@@ -147,11 +147,11 @@ var content = {
 			"description": "Exvana was a very cool project to be a part of. I'm very pleased with how the final design and site came out. Communication with the backend devs was critical as the sites search and listing functionality had to be thoughtfully tied into the front end views and design. There was a real depth of interactivity as well with the site adding to the complexity, but also creating fun challenges along the way.",
 			"skills": [
 				"Full Site Design",
-                "HTML5",
-                "CSS3",
-                "jQuery",
-                "Bootstrap"
-            ]
+        "HTML5",
+        "CSS3",
+        "jQuery",
+        "Bootstrap"
+      ]
 		},
 		{
 			"title": "Earthpure",
@@ -160,9 +160,9 @@ var content = {
 			"imgCount": 2,
 			"description": "This site was just an initial, well-designed blog to get the name going for a much larger project. I was simply given a design to build the front end for and then another developer on the team was charged with putting my template into Concrete, a popular CMS we use.",
 			"skills": [
-                "HTML5",
-                "CSS3"
-            ]
+        "HTML5",
+        "CSS3"
+      ]
 		},
 		{
 			"title": "Windows Phone",
@@ -171,10 +171,10 @@ var content = {
 			"imgCount": 3,
 			"description": "After working on Windows Phone 7 itself, I wanted to get back to web dev and was graciously put on the team to update and modify the site to go along with the phone. I did a fair bit of styling on elements, such as the main navigation, sidebar, and footer. The designers used strict redlines to translate exactly what spacing needed to be used on everything. This presented a good challenge as the site needed to look nearly identical in every browser down to IE7 and even a 5px difference was frowned upon. I also worked on the \"My Phone\" section which offers many features to owners of a WP7 device.",
 			"skills": [
-                "HTML5",
-                "CSS3",
-                "jQuery"
-            ]
+        "HTML5",
+        "CSS3",
+        "jQuery"
+      ]
 		},
 		{
 			"title": "Brown Bear Car Wash",
@@ -183,11 +183,11 @@ var content = {
 			"imgCount": 3,
 			"description": "",
 			"skills": [
-                "HTML5",
-                "CSS3",
-                "jQuery",
-                "AJAX"
-            ]
+        "HTML5",
+        "CSS3",
+        "jQuery",
+        "AJAX"
+      ]
 		},
 		{
 			"title": "A-America",
@@ -197,10 +197,10 @@ var content = {
 			"description": "",
 			"skills": [
 				"Full Design",
-                "HTML5",
-                "CSS3",
-                "jQuery"
-            ]
+        "HTML5",
+        "CSS3",
+        "jQuery"
+      ]
 		},
 		{
 			"title": "NCsoft",
@@ -210,8 +210,8 @@ var content = {
 			"description": "",
 			"skills": [
 				"HTML",
-                "CSS"
-            ]
+        "CSS"
+      ]
 		},
 		{
 			"title": "Guild Wars 2",
@@ -221,8 +221,8 @@ var content = {
 			"description": "",
 			"skills": [
 				"HTML",
-                "CSS"
-            ]
+        "CSS"
+      ]
 		}
 	],
 	
